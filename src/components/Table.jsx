51f69d741b3f89ba584d07111147a78aaa085dd4
Akhilender <akhilenderb9@gmail.com>
2023-09-25 22:07:20 +0530

    made the site more responsive till 280 px

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -42,51 +42,57 @@ const Table = () => {
 
   return (
     <>
-  <Card className="h-full w-full overflow-x-auto overflow-y-scroll">
+  <Card className="h-full w-full overflow-x-auto">
   <div className="min-w-full">
-    <table className="w-full table-auto text-left">
-      <thead>
-        <tr>
-          {TABLE_HEAD.map((head) => (
-            <th key={head} className="border-b border-blue-gray-100 p-4">
-              <Typography
-                variant="small"
-                color="blue-gray"
-                className="font-normal leading-none opacity-70"
+    <div className="overflow-x-auto">
+      <table className="w-full table-auto text-left">
+        <thead>
+          <tr>
+            {TABLE_HEAD.map((head) => (
+              <th
+                key={head}
+                className="border-b border-blue-gray-100 p-2 sm:p-2 md:p-2 lg:p-2 xl:p-4"
               >
-                {head}
-              </Typography>
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map(({ Username, Score, RNo }, index) => (
-          <tr key={index}>
-            <td className="p-4">
-              <Typography variant="small" color="blue-gray" className="font-normal">
-                {(page - 1) * itemsPerPage + index + 1}
-              </Typography>
-            </td>
-            <td className="p-4">
-              <Typography variant="small" className="font-normal">
-                {Username}
-              </Typography>
-            </td>
-            <td className="p-4">
-              <Typography variant="small" color="blue-gray" className="font-normal">
-                {Score || ' '}
-              </Typography>
-            </td>
-            <td className="p-4">
-              <Typography variant="small" color="blue-gray" className="font-normal">
-                {RNo || ' '}
-              </Typography>
-            </td>
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal leading-none opacity-70"
+                >
+                  {head}
+                </Typography>
+              </th>
+            ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.map(({ Username, Score, RNo }, index) => (
+            <tr key={index}>
+              <td className="p-2 sm:p-2 md:p-2 lg:p-2 xl:p-4">
+                <Typography variant="small" color="blue-gray" className="font-normal">
+                  {/* Ranking System */}
+                  {(page - 1) * itemsPerPage + index + 1}
+                </Typography>
+              </td>
+              <td className="p-2 sm:p-2 md:p-2 lg:p-2 xl:p-4">
+                <Typography variant="small" className="font-normal">
+                  {Username}
+                </Typography>
+              </td>
+              <td className="p-2 sm:p-2 md:p-2 lg:p-2 xl:p-4">
+                <Typography variant="small" color="blue-gray" className="font-normal">
+                  {Score || ' '}
+                </Typography>
+              </td>
+              <td className="p-2 sm:p-2 md:p-2 lg:p-2 xl:p-4">
+                <Typography variant="small" color="blue-gray" className="font-normal">
+                  {RNo || ' '}
+                </Typography>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   </div>
 </Card>
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
